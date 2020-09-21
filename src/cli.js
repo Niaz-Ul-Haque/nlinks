484eef2fcd38e9a76c4c7428a09590f08aa8c858
Niaz <mnuhaque@myseneca.ca>
2020-09-21 17:50:22 -0400

    Got all the links to show up, just 1 mor eoptional featuere left to add.

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import { exit } from "process";
 const chalk = require('chalk');
-const { UrlChecker } = require("broken-link-checker");
+const link = require('linkinator');
+
 
 export function cli(args){
 
@@ -14,59 +15,94 @@ export function cli(args){
     const readline = require('readline');
 
     async function processLineByLine() {
-        const fileStream = fs.createReadStream(args[2]);
+    const fileStream = fs.createReadStream(args[2]);
 
-        const rl = readline.createInterface({
-            input: fileStream,
-            crlfDelay: Infinity
-        });
+    const rl = readline.createInterface({
+        input: fileStream,
+        crlfDelay: Infinity
+    });
 
-        let i = 1;
-        for await (const line of rl) {
-            var myArray = line.replace(/<(.|\n)*?>/g, "");
-            //console.log(`${myArray}` );
+    let i = 1;
+    let msg = "";
+    for await (const line of rl) {
+        var myArray = line.replace(/<(.|\n)*?>/g, "");
+        async function simple() {
+            const results = await link.check({
+              path: myArray
+            });
+            
+        
+            switch(results.links[0].status) {
+                case 200:
+                    msg = "GOOD";
+                    console.log(chalk.green(`${i++} = ${msg} == ${results.links[0].status} => ${results.links[0].url}`));
+                    break;
+                case 404:
+                case 400:
+                    msg = "BAD";
+                    console.log(chalk.red(`${i++} = ${msg} == ${results.links[0].status} => ${results.links[0].url}`));
+                    break;
+                default:
+                    msg = "UNKNOWN";
+                    console.log(chalk.white(`${i++} = ${msg} == ${results.links[0].status} => ${results.links[0].url}`));
 
-            const siteChecker = new UrlChecker(
-                { 
-                    excludeInternalLinks: false,
-                    excludeExternalLinks: false, 
-                    filterLevel: 0,
-                    acceptedSchemes: ["http", "https"]
-                },
-                {
-                    "error": (error) => {
-                        console.error(error);
-                    },
-                    "link": (result) => {
+            }
+        
+    }
+    simple();
+    
+
+    // async function processLineByLine() {
+    //     const fileStream = fs.createReadStream(args[2]);
+
+    //     const rl = readline.createInterface({
+    //         input: fileStream,
+    //         crlfDelay: Infinity
+    //     });
+
+    //     let i = 1;
+    //     for await (const line of rl) {
+    //         var myArray = line.replace(/<(.|\n)*?>/g, "");
+    //         //console.log(`${myArray}` );
+
+    //         const siteChecker = new UrlChecker(
+    //             { 
+    //                 filterLevel: 0,
+    //                 acceptedSchemes: ["http", "https"]
+    //             },
+    //             {
+    //                 "error": (error) => {
+    //                     console.error(error);
+    //                 },
+    //                 "link": (result) => {
                     
-                        let msg = "";
-                        if(result.http.response) {
-                            switch(result.http.response.statusCode) {
-                                case 200:
-                                    msg = "GOOD";
-                                    console.log(chalk.green(`${i++} = ${msg} == ${result.http.response.statusCode} => ${result.url.original}`));
-                                    break;
-                                case 404:
-                                case 400:
-                                    msg = "BAD";
-                                    console.log(chalk.red(`${i++} = ${msg} == ${result.http.response.statusCode} => ${result.url.original}`));
+    //                     let msg = "";
+    //                     if(result.http.response) {
+    //                         switch(result.http.response.statusCode) {
+    //                             case 200:
+    //                                 msg = "GOOD";
+    //                                 console.log(chalk.green(`${i++} = ${msg} == ${result.http.response.statusCode} => ${result.url.original}`));
+    //                                 break;
+    //                             case 404:
+    //                             case 400:
+    //                                 msg = "BAD";
+    //                                 console.log(chalk.red(`${i++} = ${msg} == ${result.http.response.statusCode} => ${result.url.original}`));
+    //                                 break;
+    //                             default:
+    //                                 msg = "UNKNOWN";
+    //                                 console.log(chalk.white(`${i++} = ${msg} == ${result.http.response.statusCode} => ${result.url.original}`));
 
-                                    break;
-                                default:
-                                    msg = "UNKNOWN";
-                                    console.log(chalk.white(`${i++} = ${msg} == ${result.http.response.statusCode} => ${result.url.original}`));
-
-                            }
+    //                         }
                            
                         
-                        }
-                    }
-                }
-            );
-            siteChecker.enqueue(myArray);
-        }
+    //                     }
+    //                 }
+    //             }
+    //         );
+    //         siteChecker.enqueue(myArray);
+    //     }
+     }
     }
-
     processLineByLine();
 
 }
