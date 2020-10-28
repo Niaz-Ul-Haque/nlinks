9fd631dfbe036b68843b9575afa3582c230191ca
Niaz <mnuhaque@myseneca.ca>
2020-10-28 03:13:15 -0400

    Removed global vars, refactoring #1
    Edited variable names, refactoring #2
    
    Added custom func to ease up code space, refactoring #3

--- src/cli.js
+++ src/cli.js
@@ -7,40 +7,25 @@ const readline = require("readline");
 
 require("dotenv").config();
 
-let flag = 0;
-
 export function cli(args) {
 	let stat = "--all";
+	let flag = 0;
 
-	if (args[3] == "--ignore" && args[4]){
-		flag = 1
+	if (args[3] == "--ignore" && args[4]) {
+		flag = 1;
 	}
 
 	if (args[2] == "--help" || args[2] == "--h" || args[2] == "-h") {
-		console.log("Help Options");
-		console.log("'--v, --version, -v' : Will show the version of the cli");
-		console.log("'--h, --help, -h' : Will open up list of params you can use");
-		console.log(
-			"'--f, --files, -f' : Will show the type of files the program can format"
-		);
-		console.log("'--all' : Will ouptut all the links");
-		console.log("'--good' : Will ouptut only the working links");
-		console.log("'--bad' : Will ouptut only the bad links");
+		getHelp();
 		exit();
 	} else if (args[2] == undefined) {
-		console.log("Steps on how this cli works");
-		console.log("You should run like this 'nlinks <filename>'");
-		console.log("Run the '--help' options for more helpful information");
+		getWhenEmptyArgs();
 		exit();
 	} else if (args[2] == "--version" || args[2] == "--v" || args[2] == "-v") {
-		console.log("CLI Name: nlinks");
-		console.log("Version: ^0.1");
-		console.log("Made by: Mohammed Niaz Ul Haque");
-		console.log("Github: https://github.com/Niaz-Ul-Haque/nlinks");
+		getVersion();
 		exit();
 	} else if (args[2] == "--files" || args[2] == "--f" || args[2] == "-f") {
-		console.log("All the files are supported except for direct links.");
-		console.log("Will be adding the functon on release 0.2 or 0.3");
+		getFilesSupportedInfo();
 		exit();
 	} else if (args[3] == "--all" || args[3] == "--good" || args[3] == "--bad") {
 		stat = args[3];
@@ -60,13 +45,13 @@ export function cli(args) {
 		}
 
 		for await (const line of rl) {
-			let flag2 = 1
+			let flag2 = 1;
 			const link_reg = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/gi;
 
-			let myArray = line.match(link_reg);
-			if (myArray == null) continue;
+			let lineFromFile = line.match(link_reg);
+			if (lineFromFile == null) continue;
 
-			if( flag == 1 && flag2 == 1){
+			if (flag == 1 && flag2 == 1) {
 				const ignoreULRfile = fs.createReadStream(args[4]);
 				const lineOfUrls = readline.createInterface({
 					input: ignoreULRfile,
@@ -74,25 +59,25 @@ export function cli(args) {
 				});
 				for await (let l of lineOfUrls) {
 					let ignoreURL = l.match(link_reg);
-					if (ignoreURL != null){
-						if (ignoreURL.toString() === myArray.toString()){
-							console.log("SKIP / IGNORE")
-							flag2 = 1
+					if (ignoreURL != null) {
+						if (ignoreURL.toString() === lineFromFile.toString()) {
+							console.log("SKIP / IGNORE");
+							flag2 = 1;
 							break;
-						} else{
+						} else {
 							flag2 = 0;
 						}
 					}
 				}
-			} 
+			}
 
-			if(flag == 1  && flag2 == 1) continue;
+			if (flag == 1 && flag2 == 1) continue;
 
-			myArray = line.match(link_reg)[0];
+			lineFromFile = line.match(link_reg)[0];
 
 			async function simple() {
 				const results = await link.check({
-					path: myArray,
+					path: lineFromFile,
 				});
 				switch (results.links[0].status) {
 					case 200:
@@ -138,4 +123,33 @@ export function cli(args) {
 		}
 	}
 	processLineByLine();
+}
+
+function getHelp() {
+	console.log("Help Options");
+	console.log("'--v, --version, -v' : Will show the version of the cli");
+	console.log("'--h, --help, -h' : Will open up list of params you can use");
+	console.log(
+		"'--f, --files, -f' : Will show the type of files the program can format"
+	);
+	console.log("'--all' : Will ouptut all the links");
+	console.log("'--good' : Will ouptut only the working links");
+	console.log("'--bad' : Will ouptut only the bad links");
+}
+
+function getWhenEmptyArgs() {
+	console.log("Steps on how this cli works");
+	console.log("You should run like this 'nlinks <filename>'");
+	console.log("Run the '--help' options for more helpful information");
+}
+
+function getVersion() {
+	console.log("CLI Name: nlinks");
+	console.log("Version: ^0.1");
+	console.log("Made by: Mohammed Niaz Ul Haque");
+	console.log("Github: https://github.com/Niaz-Ul-Haque/nlinks");
+}
+function getFilesSupportedInfo() {
+	console.log("All the files are supported except for direct links.");
+	console.log("Will be adding the functon on release 0.2 or 0.3");
 }
